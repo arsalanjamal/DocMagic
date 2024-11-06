1169df9977797670e547be96b5fc5fb26d57a3c5
Arsalan Jamal <arsalanjamal@live.com>
2024-11-06 19:58:52 +0500

    Update streamlit_App.py

--- streamlit_App.py
+++ streamlit_App.py
@@ -2,8 +2,8 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-# from langchain.vectorstores import FAISS
-from langchain.vectorstores.faiss import FAISS
+from langchain.vectorstores import FAISS
+# from langchain.vectorstores.faiss import FAISS
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
